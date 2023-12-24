42470ebf36f64b01ae4722b258e4e24dd073fe5a
JH <140699318+Jilani-H@users.noreply.github.com>
2023-12-24 11:28:29 +0600

    updated category page looks

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,6 +1,7 @@
 import BreadCrumb from "@/components/BreadCrumb/BreadCrumb";
 import Footer from "@/components/Footer/Footer";
 import Navigation from "@/components/Navigation";
+import CategoriesPage from "./components/CategoriesPage";
 
 const page = () => {
   return (
@@ -8,7 +9,9 @@ const page = () => {
       <Navigation />
       <BreadCrumb />
       <section className="w-full flex flex-col flex-1 items-center ">
-        <div className="page-style-container h-full">Category page</div>
+        <div className="page-style-container h-full">
+          <CategoriesPage/>
+          </div>
       </section>
       <Footer/>
     </>
---
+++ src/components/Product/FilteredProducts/FilterProducts.tsx
@@ -0,0 +1,16 @@
+import { prodData } from "@/lib/demodata"
+
+type FilterProductsTypes = {
+data: string
+}
+const FilterProducts = ({data}: FilterProductsTypes) => {
+    const products = prodData.filter((item)=>{
+        return item.category === data
+    })
+    console.log(products);
+    
+  return (
+    <div>FilterProducts</div>
+  )
+}
+export default FilterProducts
